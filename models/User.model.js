3dba6aee551ca1d6c1800bde57bf3d3b9ef3c762
Krists Krievins <krists.krievins@gmail.com>
2025-04-08 10:02:47 +0200

    Setup a new server

---
+++ models/User.model.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "User name is required."],
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+    profileimage: {
+      type: String,
+    },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
---
+++ routes/auth.routes.js
@@ -0,0 +1,63 @@
+const router = require("express").Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/User.model");
+
+// Create a route to make a user with an
+router.post("/signup", async (req, res) => {
+  try {
+    const salt = bcryptjs.genSaltSync();
+    const hashedPassword = bcryptjs.hashSync(req.body.password, salt);
+    const hashedUser = {
+      ...req.body,
+      password: hashedPassword,
+    };
+    const newUser = await UserModel.create(hashedUser);
+    console.log("user created successfully", newUser);
+    res.status(201).json({ message: "User successfully create in DB" });
+  } catch {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Find user based on their e-mail
+router.post("/login", async (req, res) => {
+  try {
+    // Finding a user
+    const foundUser = await UserModel.findOne({ email: req.body.email });
+    if (!foundUser) {
+      res.status(400).json({ errorMessage: "E-mail not found" });
+    } else {
+      // Compare passwords when found user
+      const passwordFromFrontend = req.body.password;
+      const passwordHashedInDB = foundUser.password;
+      const passwordsMatch = bcryptjs.compareSync(
+        passwordFromFrontend,
+        passwordHashedInDB
+      );
+      console.log("Match?", passwordsMatch);
+      if (!passwordsMatch) {
+        res.status(400).json({ errorMessage: "Password did not match" });
+      } else {
+        // Add non secret data
+        const data = { _id: foundUser.id, username: foundUser.username };
+        const authToken = jwt.sign(data, process.env.TOKEN_SECRET, {
+          algorithm: "HS256",
+          expiresIn: "24h",
+        });
+        res
+          .status(200)
+          .json({
+            message: "Password Matched and you are Logged in",
+            authToken,
+          });
+      }
+    }
+  } catch {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
