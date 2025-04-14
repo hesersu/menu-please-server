109c447639c3281884979f35887643d81cb6eaec
Christian Wirsching <christianwirsching@gmail.com>
2025-04-14 14:25:43 +0200

    populate restaurants after menu is created

--- routes/menus.routes.js
+++ routes/menus.routes.js
@@ -49,8 +49,9 @@ router.get("/all-menus-one-user", async (req, res) => {
   const { ownerId } = req.query
   console.log("this is the userId", ownerId)
   try {
-    const menuResponse = await MenuModel.find({owner_id: new mongoose.Types.ObjectId(ownerId)});
-    res.status(200).json({ allMenusForOneUser: menuResponse });
+    const menuResponse = await MenuModel.find({owner_id: new mongoose.Types.ObjectId(ownerId)})
+    .populate("restaurant_id");
+    res.status(200).json({ allMenusForOneUser: menuResponse });n
   } catch (err) {
     console.log(err);
     res.status(500).json({ errorMessage: "Trouble getting menus" });
