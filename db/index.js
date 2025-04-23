9491d89498e8623ead9787095e68c0889220b93b
Krists Krievins <krists.krievins@gmail.com>
2025-04-23 12:21:31 +0200

    Fixed DB Load before Listener

--- db/index.js
+++ db/index.js
@@ -8,12 +8,18 @@ const mongoose = require("mongoose");
 const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/menu-please-db";
 
-mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    const dbName = x.connections[0].name;
-    console.log(`Connected to Mongo! Database name: "${dbName}"`);
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
+const withDB = (serverListener) =>
+  mongoose
+    .connect(MONGO_URI)
+    .then((x) => {
+      const dbName = x.connections[0].name;
+      console.log(`Connected to Mongo! Database name: "${dbName}"`);
+      if (typeof serverListener === "function") {
+        serverListener();
+      }
+    })
+    .catch((err) => {
+      console.error("Error connecting to mongo: ", err);
+    });
+
+module.exports = withDB;
